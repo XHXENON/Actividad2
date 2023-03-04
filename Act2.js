40a411d66843675824b9f77a794255452ca4eb1b
XHXENON <124637361+XHXENON@users.noreply.github.com>
2023-03-03 20:43:10 -0600

    Add files via upload

---
+++ Act2.js
@@ -0,0 +1,17 @@
+unicoProgramador={
+    name: "Javier Luna",
+    age:19,
+    programmer: true,
+    Date:{
+        date1:new Date(2003,3,5,12,30,0,0)
+    },
+    favoriteBook:{
+        title: "Las 48 leyes del poder",
+        author:"Robert Greene",
+        miObjeto:{
+            date: new Date(1988,1,1,12,0,0)
+        },
+        URL:"https://es.wikipedia.org/wiki/Las_48_leyes_del_poder"
+    }
+}
+console.log(unicoProgramador);
